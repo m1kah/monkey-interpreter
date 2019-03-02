be96970dc8e0b40a40fae937e16018d4339334d5
Mika Hamalainen <mika.hamalainen@gmail.com>
2019-03-02 08:52:24 +0200

    Start for parser with test

--- src/ast/ast.go
+++ src/ast/ast.go
@@ -1,5 +1,9 @@
 package ast
 
+import (
+    "token"
+)
+
 type Node interface {
     TokenLiteral() string
 }
@@ -24,5 +28,26 @@ func (p *Program) TokenLiteral() string {
     } else {
         return ""
     }
+}
+
+type LetStatement struct {
+    Token token.Token
+    Name *Identifier
+    Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string {
+    return ls.Token.Literal
+}
+
+type Identifier struct {
+    Token token.Token
+    Value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+    return i.Token.Literal
 }
 
