b64f071a754bff902bca88f3e8577f79cdb56ec7
Mika Hamalainen <mika.hamalainen@gmail.com>
2019-07-21 11:02:17 +0300

    Token for string type

--- src/lexer/lexer_test.go
+++ src/lexer/lexer_test.go
@@ -7,20 +7,109 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-    input := `=+(){},;`
+    input := `
+        let five = 5;
+        let ten = 10;
+        
+        let add = fn(x, y) {
+            x + y;
+        };
+
+        let result = add(five, ten);
+        !-/*5;
+        5 < 10 > 5;
+
+        if (5 < 10) {
+            return true;
+        } else {
+            return false;
+        }
+
+        10 == 10;
+        10 != 9;
+        "foobar"
+        "foo bar"
+    `
 
     tests := []struct {
         expectedType    token.TokenType
         expectedLiteral string
     }{
+        {token.LET, "let"},
+        {token.IDENT, "five"},
         {token.ASSIGN, "="},
-        {token.PLUS, "+"},
-        {token.LPAREN, "("},
-        {token.RPAREN, ")"},
-        {token.LBRACE, "{"},
-        {token.RBRACE, "}"},
+        {token.INT, "5"},
+        {token.SEMICOLON, ";"},     // 5
+        {token.LET, "let"},
+        {token.IDENT, "ten"},
+        {token.ASSIGN, "="},
+        {token.INT, "10"},
+        {token.SEMICOLON, ";"},     // 10
+        {token.LET, "let"},
+        {token.IDENT, "add"},
+        {token.ASSIGN, "="},
+        {token.FUNCTION, "fn"},
+        {token.LPAREN, "("},        // 15
+        {token.IDENT, "x"},
         {token.COMMA, ","},
+        {token.IDENT, "y"},
+        {token.RPAREN, ")"},
+        {token.LBRACE, "{"},        // 20
+        {token.IDENT, "x"},
+        {token.PLUS, "+"},
+        {token.IDENT, "y"},
         {token.SEMICOLON, ";"},
+        {token.RBRACE, "}"},        // 25
+        {token.SEMICOLON, ";"},
+        {token.LET, "let"},
+        {token.IDENT, "result"},
+        {token.ASSIGN, "="},
+        {token.IDENT, "add"},       // 30
+        {token.LPAREN, "("},
+        {token.IDENT, "five"},
+        {token.COMMA, ","},
+        {token.IDENT, "ten"},
+        {token.RPAREN, ")"},        // 35
+        {token.SEMICOLON, ";"},
+        {token.BANG, "!"},
+        {token.MINUS, "-"},
+        {token.SLASH, "/"},
+        {token.ASTERISK, "*"},      // 40
+        {token.INT, "5"},
+        {token.SEMICOLON, ";"},
+        {token.INT, "5"},
+        {token.LT, "<"},
+        {token.INT, "10"},          // 45
+        {token.GT, ">"},
+        {token.INT, "5"},
+        {token.SEMICOLON, ";"},
+        {token.IF, "if"},
+        {token.LPAREN, "("},        // 50
+        {token.INT, "5"},
+        {token.LT, "<"},
+        {token.INT, "10"},
+        {token.RPAREN, ")"},
+        {token.LBRACE, "{"},        // 50
+        {token.RETURN, "return"},
+        {token.TRUE, "true"},
+        {token.SEMICOLON, ";"},
+        {token.RBRACE, "}"},
+        {token.ELSE, "else"},       // 55
+        {token.LBRACE, "{"},
+        {token.RETURN, "return"},
+        {token.FALSE, "false"},
+        {token.SEMICOLON, ";"},
+        {token.RBRACE, "}"},        // 60
+        {token.INT, "10"},
+        {token.EQ, "=="},
+        {token.INT, "10"},
+        {token.SEMICOLON, ";"},
+        {token.INT, "10"},          // 65
+        {token.NOT_EQ, "!="},
+        {token.INT, "9"},
+        {token.SEMICOLON, ";"},
+        {token.STRING, "foobar"},
+        {token.STRING, "foo bar"},  // 70
         {token.EOF, ""},
     }
 
